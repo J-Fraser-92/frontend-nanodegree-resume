2bd8f4f6bb17982b1699eafd7d51a28b2b1933a6
James Fraser <james.fraser@xedosoftware.com>
2016-10-07 15:29:51 +0100

    Adding

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,36 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("James Fraser");
+
+var bio = {
+	"name": "James Fraser",
+	"role": "Automation Engineer",
+	"contact": {
+		"email": "james_fraser@outlook.com",
+		"phone": "07887 505767"	
+	}	
+}
+
+var education = {
+	"degrees": [
+		{
+			"university": "University of Strathclyde",
+			"course": "Computer & Electronic Systems",
+			"level": "MEng",
+			"startDate": "September 2010",
+			"endDate": "July 2015"
+		}
+	]
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedNumber = HTMLmobile.replace("%data%", bio.contact.phone);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+$("#header").append(formattedEmail);
+$("#header").append(formattedNumber);
+
+
